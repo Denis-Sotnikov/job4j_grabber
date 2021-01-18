49bffbadd0a87b89df862cb3a14bf424a63b78a3
Denis Sotnikov <sport_piter@mail.ru>
2021-01-18 08:29:42 +0300

    2.2. Модель данных - Post. [#437713]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,12 @@
+package ru.job4j.html;
+
+import java.util.Date;
+
+public class Post {
+    private int id;
+    private String text;
+    private String autor;
+    private int quantityAnswers;
+    private Date dateOfCreation;
+    private int numberOfViews;
+}
